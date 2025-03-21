204178b2c0f15b58d9fda0723685a290051a36f0
Nilson Cesar <62442496+NilsonCesar@users.noreply.github.com>
2025-03-21 08:51:09 -0300

    Implement rename and add content functions

--- 04_file_system/app.js
+++ 04_file_system/app.js
@@ -1,7 +1,7 @@
 const fs = require('fs/promises');
 
 (async () => {
-  const CREATE_FILE = 'create a file';
+  const CREATE_FILE = 'create the file';
   const DELETE_FILE = 'delete the file';
   const RENAME_FILE = 'rename the file';
   const ADD_TO_FILE = 'add to the file';
@@ -19,16 +19,50 @@ const fs = require('fs/promises');
     }
   }
 
-  const deleteFile = async filePath => {}
- 
-  const addToFile = async (filePath, content) => {
+  const deleteFile = async filePath => {
     filePath = filePath.trim();
-    content = content.trim();
-  } 
+    try {
+	await fs.unlink(filePath);
+	console.log('File succesfully deleted');
+    } catch (e) {
+	if (e.code === 'ENOENT') {
+          console.log('File not found');
+	}
+	else {
+	  console.log('An error occuried.');
+	  console.log(e);
+	}
+    }
+  }
 
-  const renameFile = async (oldPath, newPath) => {
+   const renameFile = async (oldPath, newPath) => {
     oldPath = oldPath.trim();
     newPath = newPath.trim();
+    try {
+      await fs.rename(oldPath, newPath);
+    } catch (e) {
+      if (e.code === 'ENOENT') {
+	console.log('File not found');
+      } else {
+	console.log('An error occurs');
+	console.log(e);
+      }
+    }
+  }
+ 
+  let addedContent;
+  const addToFile = async (filePath, content) => {
+    if (addedContent === content) return;
+    filePath = filePath.trim();
+    content = content.trim();
+    addedContent = content;
+    try {
+      const fileToWrite = await fs.open(filePath, 'a');
+      fileToWrite.write(content); 
+    } catch (e) {
+      console.log('An error occurs');
+      consoel.log(e);
+    }
   }
 
   const fileHandler = await fs.open('./command.txt', 'r');
@@ -49,6 +83,20 @@ const fs = require('fs/promises');
 	console.log(filePath);
 	createFile(filePath);	
       }
+	
+      if (textContent.includes(DELETE_FILE)) {
+	const filePath = textContent.substring(DELETE_FILE.length + 1);
+	console.log(filePath);
+        deleteFile(filePath);
+      }
+      
+      if (textContent.includes(RENAME_FILE)) {
+	const strDivisor = ' to ';
+	const divisor = textContent.indexOf(strDivisor);
+	const oldFilePath = textContent.substring(RENAME_FILE.length + 1, divisor);
+	const newFilePath = textContent.substring(divisor + strDivisor.length);
+	renameFile(oldFilePath, newFilePath);
+      }
 
       if (textContent.includes(ADD_TO_FILE)) {
 	const strDivisor = ' this content: ';
@@ -58,13 +106,6 @@ const fs = require('fs/promises');
 	addToFile(filePath, content);
       }
 
-      if (textContent.includes(RENAME_FILE)) {
-	const strDivisor = ' to ';
-	const divisor = textContent.indexOf(strDivisor);
-	const oldFilePath = textContent.substring(RENAME_FILE.length + 1, divisor);
-	const newFilePath = textContent.substring(divisor + strDivisor.length);
-	renameFile(oldFilePath, newFilePath);
-      }
     }
   )
      
