566316576ba111fb351ada406015db7d8ce55e99
nilsoncesar <admncoaf@gmail.com>
2025-04-06 08:30:00 -0300

    Add functionality of send message to other users in chat

--- 06_networking/chat_app/server.js
+++ 06_networking/chat_app/server.js
@@ -2,9 +2,19 @@ const net = require('net');
 
 const server = net.createServer();
 
+const clients = [];
+
 server.on('connection', socket => {
     console.log('A new conection to the server');
-})
+
+    socket.on('data', data => {
+        clients.map(socket => {
+            socket.write(data.toString('utf-8'));
+	});
+    });
+
+    clients.push(socket);
+});
 
 server.listen(3008, '127.0.0.1', () => {
     console.log(`Opened server on`, server.address());
