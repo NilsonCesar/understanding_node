d27381c87e023759e46c378095445a0bd32045d5
nilsoncesar <admncoaf@gmail.com>
2025-04-06 08:57:35 -0300

    Improve ui of chat app

--- 06_networking/chat_app/client.js
+++ 06_networking/chat_app/client.js
@@ -6,15 +6,43 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+const clearLine = dir => {
+    return new Promise((resolve, reject) => {
+        process.stdout.clearLine(dir, () => {
+            resolve();
+        });
+    })
+}
+
+const moveCursor = (dx, dy) => {
+    return new Promise((resolve, reject) => {
+        process.stdout.moveCursor(dx, dy, () => {
+            resolve();
+        });
+    })
+}
+
 const socket = net.createConnection({port: 3008, host: '127.0.0.1'}, async () => {
     console.log('Connected to the server');
 
-    const message = await rl.question('Enter a message > ');
-    socket.write(message);
-});
+    const ask = async () => {    
+        const message = await rl.question('Enter a message > ');
+        await moveCursor(0, -1);
+        await clearLine(0);
+        socket.write(message);
+    }
 
-socket.on('data', data => {
-    console.log(data.toString('utf-8'));
+    ask();
+
+
+    socket.on('data', async data => {
+        console.log();
+        await moveCursor(0, -1);
+        await clearLine(0);
+    
+        console.log(data.toString('utf-8'));
+        ask();
+    });
 });
 
 socket.on('close', () => {
