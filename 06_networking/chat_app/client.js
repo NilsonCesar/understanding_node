91ce19e2c3878a78da85375997779af6d04f680e
nilsoncesar <admncoaf@gmail.com>
2025-04-08 21:55:03 -0300

    Put labels in users

--- 06_networking/chat_app/client.js
+++ 06_networking/chat_app/client.js
@@ -1,6 +1,8 @@
 const net = require('net');
 const readline = require('readline/promises');
 
+let id;
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -29,18 +31,26 @@ const socket = net.createConnection({port: 3008, host: '127.0.0.1'}, async () =>
         const message = await rl.question('Enter a message > ');
         await moveCursor(0, -1);
         await clearLine(0);
-        socket.write(message);
+        socket.write(`${id}-message-${message}`);
     }
 
     ask();
 
 
     socket.on('data', async data => {
-        console.log();
-        await moveCursor(0, -1);
-        await clearLine(0);
+	if (data.toString('utf-8').substring(0, 2) === 'id') {
+	    id = data.toString('utf-8').substring(3);
+
+	    console.log(`Your id is ${id}!\n`);
+	}
+	else {
+            console.log();
+            await moveCursor(0, -1);
+            await clearLine(0);
     
-        console.log(data.toString('utf-8'));
+            console.log(data.toString('utf-8'));
+	}
+	
         ask();
     });
 });
