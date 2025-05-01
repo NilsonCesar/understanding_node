6bd681a35c7043306a59b0f73cf96ac9314e3308
nilsoncesar <admncoaf@gmail.com>
2025-05-01 16:17:24 -0300

    Add example of load balance with proxy

---
+++ 07_http/poster-proxy/server_2.js
@@ -0,0 +1,93 @@
+const Capuccino = require('../capuccino');
+
+const USERS = [
+  {
+    id: 1,
+    name: 'Uncle Bob',
+    username: 'bob123',
+    password: 'clean',
+  },
+  {
+    id: 2,
+    name: 'Alan Turing',
+    username: 'alan',
+    password: 'prove',
+  },
+  {
+    id: 3,
+    name: 'Godard',
+    username: 'imnotauser',
+    password: 'typicallyfrench',
+  },
+];
+
+const POSTS = [
+  {
+    id: 1,
+    title: 'The first post in poster',
+    body: 'Definitively, it isnt an elephant video',
+    userId: 1,
+  },
+];
+
+const PORT = 9002;
+const server = new Capuccino();
+
+// ----- Files Routes ----- //
+
+server.route('post', '/api/login', (req, res) => {
+  let body = '';
+  req.on('data', chunk => {
+    body += chunk.toString();
+  });
+
+  req.on('end', () => {
+    body = JSON.parse(body);
+    
+    const username = body.username;
+    const password = body.password;
+    const user = USERS.find(user => user.username === username);
+    
+    if (user && user.password === password) {
+      res.status(200).json({ message: 'logged in successfully' });
+    } else {
+      res.status(401).json({ error: 'Invalid user or password invalid.' })
+    }
+  });
+});
+
+// Send the the list of all post that we have
+server.route('get', '/', (req, res) => {
+  console.log('Server 2 is handling a request!');
+  res.sendFile('./public/index.html', 'text/html');
+});
+
+server.route('get', '/login', (req, res) => {
+  res.sendFile('./public/index.html', 'text/html');
+});
+
+server.route('get', '/api/user', (req, res) => {
+
+});
+
+server.route('get', '/styles.css', (req, res) => {
+  res.sendFile('./public/styles.css', 'text/css');
+});
+
+server.route('get', '/scripts.js', (req, res) => {
+  res.sendFile('./public/scripts.js', 'text/javascript');
+});
+
+// ----- Json Routes ----- //
+server.route('get', '/api/posts', (req, res) => {
+  const posts = POSTS.map(post => {
+    const user = USERS.find(user => user.id === post.userId);
+    post.author = user.name;
+    return post;
+  });
+  res.status(200).json(posts);
+});
+
+server.listen(PORT, () => {
+  console.log('Server has started on port', PORT);
+});
