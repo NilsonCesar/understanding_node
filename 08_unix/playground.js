4fd7ca681a49725809cdc455f3faa8fa9c462ad1
nilsoncesar <admncoaf@gmail.com>
2025-05-07 19:14:17 -0300

    Add playgroud to play with spawn and exec =)

---
+++ 08_unix/playground.js
@@ -0,0 +1,17 @@
+const { spawn, exec } = require('node:child_process');
+
+const subprocess = spawn('ls');
+
+subprocess.stdout.on('data', (data) => {
+  console.log(data.toString());
+});
+
+exec("echo 'something string' | tr ' ' '\n' ", (err, stdout, stderr) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(stdout);
+  console.log('stderr: ' + stderr);
+});
