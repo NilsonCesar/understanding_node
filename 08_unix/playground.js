82b81139b22a7e8a1fddeeaee9351abc2d4de256
nilsoncesar <admncoaf@gmail.com>
2025-05-11 17:36:42 -0300

    Add code related to env and process studies

--- 08_unix/playground.js
+++ 08_unix/playground.js
@@ -1,9 +1,12 @@
 const { spawn, exec } = require('node:child_process');
 
+console.log(process.argv);
+
 const subprocess = spawn('ls', ['-alF']);
 
 subprocess.stdout.on('data', (data) => {
   console.log(data.toString());
+  console.log();
 });
 
 exec("echo 'something string' | tr ' ' '\n' ", (err, stdout, stderr) => {
