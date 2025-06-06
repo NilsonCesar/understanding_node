6483f3cfc9e68a1c5f8a293d24f57cd1c10fe218
nilsoncesar <admncoaf@gmail.com>
2025-06-06 19:31:01 -0300

    Add more examples in thread app

--- 10_threads/app.js
+++ 10_threads/app.js
@@ -1,9 +1,9 @@
-const { Worker } = require('worker_threads');
+const { Worker, MessageChannel } = require('worker_threads');
 
-const a = 400;
+// const a = 400;
 
-const thread0 = new Worker('./calc.js');
-const thread1 = new Worker('./calc.js');
+// const thread0 = new Worker('./calc.js', { workerData: 'text' });
+// const thread1 = new Worker('./calc.js');
 
 // It will be blocked!
 // setTimeout(
@@ -12,11 +12,70 @@ const thread1 = new Worker('./calc.js');
 //         console.log('File was created');
 // }), 3000);
 
-while (true) {}
+// while (true) {}
 
-setTimeout(() => {
-    const thread2 = new Worker('./calc.js');
-}, 500);
+// setTimeout(() => {
+//     const thread2 = new Worker('./calc.js');
+// }, 500);
 
-console.log(a);
-console.log(process.pid);
+// console.log(a);
+// console.log(process.pid);
+
+// const messageChannel = new MessageChannel();
+// console.log(messageChannel);
+
+// const port1 = messageChannel.port1;
+// const port2 = messageChannel.port2;
+
+// port1.postMessage({ name: 'Joe' });
+// port2.on('message', msg => {
+//     console.log('Message received from port2:', msg);
+// });
+
+
+// const { port1, port2 } = new MessageChannel();
+
+// const thread1 = new Worker('./calc.js', {
+//     workerData: { port: port1 },
+//     transferList: [port1]
+// });
+
+// const thread2 = new Worker('./calc.js', {
+//     workerData: { port: port2 },
+//     transferList: [port2]
+// });
+
+
+// const channelThread1 = new MessageChannel();
+// const port_main_1 = channelThread1.port1;
+// const port1 = channelThread1.port2;
+
+// const channelThread2 = new MessageChannel();
+// const port_main_2 = channelThread2.port1;
+// const port2 = channelThread2.port2;
+
+// port_main_1.on('message', msg => {
+//     console.log('Message received from thread1:', msg);
+// });
+
+// port_main_2.on('message', msg => {
+//     console.log('Message received from thread2:', msg);
+// });
+
+// const thread1 = new Worker('./calc.js', {
+//     workerData: { port: port1, id: 'from thread1!' },
+//     transferList: [port1]
+// });
+
+// const thread2 = new Worker('./calc.js', {
+//     workerData: { port: port2, id: 'from thread2!'},
+//     transferList: [port2]
+// });
+
+const thread1 = new Worker('./calc.js');
+
+thread1.on('message', msg => {
+    console.log('Main thread receive this message:', msg);
+});
+
+thread1.postMessage({ letra: 'Maracatu atomicooo' });
